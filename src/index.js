077fbcd9bb7e0e5061eeda8e56c0f5c0e7efcd39
mfrb15 <129767020+mfrb15@users.noreply.github.com>
2023-04-19 09:50:36 -0400

    valida tarjeta y muestra maskify

--- src/index.js
+++ src/index.js
@@ -1,3 +1,89 @@
 import validator from './validator.js';
-
 console.log(validator);
+//Declarando variables. 
+let creditCardNumber = "";
+const botonEnviar = document.getElementById("enviar");
+const sectionPadre = document.getElementById("padre");
+const h1tdc = document.getElementById("h1Tdc");
+const inputName = document.getElementById("input-name");
+const inputNumber = document.getElementById("input-number");
+const inputMonth = document.getElementById("input-month");
+const inputYear = document.getElementById("input-year");
+const numerodeTarjeta = document.getElementById("numerodeTarjeta");
+const nombredeTarjeta = document.getElementById("nombreDeTarjeta");
+
+//aplicando una función 
+botonEnviar.addEventListener("click", () => {
+  //Declarando una variable dentro de una función y extrayendo su valor. (ingresado por el usuario)
+  creditCardNumber = document.getElementById("input-number").value;
+  console.log(creditCardNumber); //mostrando este valor en la consola. 
+
+  // Declarando la variable tarjeta validada para poder reversar su numero y usarlo para
+  //reversarlo con el método .reverse que esta en validator.
+  let cardValid = validator.isValid(creditCardNumber);
+  if (cardValid == true) {
+    h1tdc.innerHTML = "Resultado de la operación";
+    sectionPadre.innerHTML = `<section class="maskify">
+      <div class="pagoProcesado" id="pagoAprobado">
+      <img src="../images/aprobado.png" class="iconoAprobado" alt="icono de Aprobado">
+      <p class="mensajeGraciasporsucompra">¡Gracias por tu compra!</p>
+      <p id="input-number-maskify"></p>
+      </section>`;
+    let maskify = validator.maskify(creditCardNumber);
+    const pCardNumber = document.getElementById("input-number-maskify");
+    pCardNumber.innerHTML = "Operación validada para tu tarjeta " + maskify;
+    console.log(maskify);
+  } else {
+    h1tdc.innerHTML = "Resultado de la operación";
+    sectionPadre.innerHTML = `<section class="maskify">
+      <div class="pagoProcesado" id="pagoAprobado">
+      <img src="../images/rechazado.png" class="iconoAprobado" alt="icono de Aprobado">
+      <p class="mensajeGraciasporsucompra">¡Error, Tarjeta Inválida!</p>
+      <p id="input-number-maskify"></p>
+      </section>`;
+  }
+}
+)
+// tarjeta valida
+//4532314509432211
+
+// Logica para Css
+
+//interaccion tarjeta
+
+
+
+inputName.addEventListener("input", () => {
+  nombredeTarjeta.innerText = inputName.value;
+  if (inputName.value.length === 0) {
+    nombredeTarjeta.innerText = "Mafer Rodriguez";
+  }
+});
+
+inputNumber.addEventListener("input", () => {
+  numerodeTarjeta.innerText = inputNumber.value;
+  if (inputNumber.value.length === 0) {
+    numerodeTarjeta.innerText = "0000 0000 0000 0000";
+  }
+}
+)
+
+inputMonth.addEventListener("input", () => {
+  mes.innerText = inputMonth.value;
+  if (inputMonth.value.length === 0) {
+    mes.innerText = "00";
+  }
+}
+)
+
+inputYear.addEventListener("input", () => {
+  año.innerText = inputYear.value;
+  if (inputYear.value.length === 0) {
+    año.innerText = "00";
+  }
+}
+)
+
+
+
+
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,78 @@
+//Esto es un objeto. 
 const validator = {
-  // ...
-};
+  // ... 
+  //Esto es un método
+  isValid(creditCardNumber) {
+    //let nombre = "1234 1234 1234 1234";
+    // El método split corta una cadena de texto o string, para separarla y poderla contar.
+    let arrayCard = creditCardNumber.split(""); //("")--->Porque no tengo ni comas ni espacios, si hubiera , seria (",")
+    console.log(arrayCard);
+    let reversed = arrayCard.reverse();
+
+    console.log(reversed);
+    //Multiplicar Pares por 2
+    for (let i = 0; i < reversed.length; i++) {
+      if (i % 2 != 0) {
+        reversed[i] = reversed[i] * 2
+
+
+        if (reversed[i] >= 10) {
+          // 1 0 
+          let dosDigitosSumados = 0;
+          let numeroDosdigitos = reversed[i].toString().split("");
+          console.log("numero 2 digitos " + numeroDosdigitos)
+          for (let i = 0; i < numeroDosdigitos.length; i++) {
+            dosDigitosSumados = dosDigitosSumados + parseInt(numeroDosdigitos[i]);
+          }
+          reversed[i] = dosDigitosSumados;
+          console.log(dosDigitosSumados + " esta es mi suma de dos digitos")
+          dosDigitosSumados = 0;
+        }
+      }
+    }
+    console.log(reversed + "Esta es la multiplicación");
+
+    // Sumar todos los elementos 
+
+    let digitosSumados = 0;
+
+    for (let i = 0; i < reversed.length; i++) {
+      digitosSumados = digitosSumados + parseInt(reversed[i]);
+      console.log(typeof (digitosSumados))
+    }
+
+    console.log(digitosSumados + "esta es mi suma")
+
+    // validar que el residuo de 10 sea igual a cero
+    let validCard = false;
+    if (digitosSumados % 10 == 0) {
+      validCard = true;
+    } else {
+      validCard = false;
+    }
+    return validCard;
+  },
+
+  maskify(creditCardNumber) {
+
+    creditCardNumber = creditCardNumber.toString();
+
+    let acumulador = "";
+    for (let i = 0; i < creditCardNumber.length; i++) {
+      if (i > 11) {
+        acumulador = acumulador + creditCardNumber[i]
+      }
+      else {
+        acumulador = acumulador + "#"
+
+      }
+    }
+    return acumulador;
+  },
+}
+
+
+
+
 
 export default validator;
