a4cbf98657abcb9e036b956134dee946025c98eb
mfrb15 <129767020+mfrb15@users.noreply.github.com>
2023-04-25 12:24:30 -0400

    Updates

--- index.js
+++ index.js
@@ -1,20 +1,92 @@
 import validator from './validator.js';
-console.log(validator);
-        let tarjeta ="";
+//Declarando variables. 
+let creditCardNumber = "";
 const botonEnviar = document.getElementById("enviar");
-const numeroTarjeta = document.getElementById("input-number");
+const sectionPadre = document.getElementById("padre");
+const h1tdc = document.getElementById("h1Tdc");
+const inputName = document.getElementById("input-name");
+const inputNumber = document.getElementById("input-number");
+const inputMonth = document.getElementById("input-month");
+const inputYear = document.getElementById("input-year");
+const numerodeTarjeta = document.getElementById("numerodeTarjeta");
+const nombredeTarjeta = document.getElementById("nombreDeTarjeta");
 
+//aplicando una función 
 botonEnviar.addEventListener("click", () => {
-    tarjeta = document.getElementById("input-number").value;
-    console.log(tarjeta);
-    let tarjetaValidada = validator.reversarNumero(tarjeta);
-    console.log("esta es mi tarjeta reversada " + tarjetaValidada)
-  
+  //Declarando una variable dentro de una función y extrayendo su valor. (ingresado por el usuario)
+  creditCardNumber = document.getElementById("input-number").value;
+  // console.log(creditCardNumber); //mostrando este valor en la consola. 
+
+
+  // Declarando la variable tarjeta validada para poder reversar su numero y usarlo para
+  //reversarlo con el método .reverse que esta en validator.
+  const cardValid = validator.isValid(creditCardNumber);
+
+  if (inputYear.value, inputMonth.value, inputName.value, inputNumber.value === "") {
+    alert("Debe rellenar todos los campos");
+  }
+  else {
+    if (cardValid === true) {
+      h1tdc.innerHTML = "Resultado de la operación";
+      sectionPadre.innerHTML = `<section class="maskify">
+   <div class="pagoProcesado" id="pagoAprobado">
+   <img src="./images/aprobado.png" class="iconoAprobado" alt="icono de Aprobado">
+   <p class="mensajeGraciasporsucompra">¡Gracias por tu compra!</p>
+   <p id="input-number-maskify"></p>
+   </section>`;
+      const maskify = validator.maskify(creditCardNumber);
+      const pCardNumber = document.getElementById("input-number-maskify");
+      pCardNumber.innerHTML = "Operación validada para tu tarjeta " + maskify;
+      // console.log(maskify);
+    } else {
+      h1tdc.innerHTML = "Resultado de la operación";
+      sectionPadre.innerHTML = `<section class="maskify">
+   <div class="pagoProcesado" id="pagoAprobado">
+   <img src="./images/rechazado.png" class="iconoAprobado" alt="icono de Aprobado">
+   <p class="mensajeGraciasporsucompra">¡Error, Tarjeta Inválida!</p>
+   <p id="input-number-maskify"></p>
+   </section>`;
+    }
+
+  }
+
+}
+)
+// tarjeta valida
+//4532314509432211
+
+// Logica para Css
+
+//interaccion tarjeta
+
+inputName.addEventListener("input", () => {
+  nombredeTarjeta.innerText = inputName.value;
+  if (inputName.value.length === 0) {
+    nombredeTarjeta.innerText = "Mafer Rodriguez";
+  }
+});
+
+inputNumber.addEventListener("input", () => {
+  numerodeTarjeta.innerText = inputNumber.value;
+  if (inputNumber.value.length === 0) {
+    numerodeTarjeta.innerText = "0000 0000 0000 0000";
+  }
 }
 )
 
+inputMonth.addEventListener("input", () => {
+  if (inputMonth.value.length === 0) {
+    inputMonth.innerText = "00";
+  }
+}
+)
 
-
+inputYear.addEventListener("input", () => {
+  if (inputYear.value.length === 0) {
+    inputYear.innerText = "00";
+  }
+}
+)
 
 
 
--- validator.js
+++ validator.js
@@ -1,14 +1,66 @@
+//Esto es un objeto. 
 const validator = {
   // ... 
- reversarNumero(tarjeta){
-  //let nombre = "1234 1234 1234 1234";
-  let arrayTarjeta = tarjeta.split("");
-  console.log(arrayTarjeta);
-  let reversed = arrayTarjeta.reverse();
-  console.log(reversed);
-  return reversed; 
- }
-};
+  //Esto es un método
+  isValid(creditCardNumber) {
+    //let nombre = "1234 1234 1234 1234";
+    // El método split corta una cadena de texto o string, para separarla y poderla contar.
+    const arrayCard = creditCardNumber.split(""); //("")--->Porque no tengo ni comas ni espacios, si hubiera , seria (",")
+    // console.log(arrayCard);
+    const reversed = arrayCard.reverse();
+
+    // console.log(reversed);
+    //Multiplicar Pares por 2
+    for (let i = 0; i < reversed.length; i++) {
+      if (i % 2 !== 0) {
+        reversed[i] = reversed[i] * 2
+
+
+        if (reversed[i] >= 10) {
+          // 1 0 
+          let dosDigitosSumados = 0;
+          const numeroDosdigitos = reversed[i].toString().split("");
+          // console.log("numero 2 digitos " + numeroDosdigitos)
+          for (let i = 0; i < numeroDosdigitos.length; i++) {
+            dosDigitosSumados = dosDigitosSumados + parseInt(numeroDosdigitos[i]);
+          }
+          reversed[i] = dosDigitosSumados;
+          // console.log(dosDigitosSumados + " esta es mi suma de dos digitos")
+          dosDigitosSumados = 0; //es para que se resetee la variable para que se sumen solo los 2 números que entran al if.
+        }
+      }
+    }
+    // console.log(reversed + "Esta es la multiplicación");
+
+    // Sumar todos los elementos 
+
+    let digitosSumados = 0;
+
+    for (let i = 0; i < reversed.length; i++) {
+      digitosSumados = digitosSumados + parseInt(reversed[i]);
+      // console.log(typeof (digitosSumados))
+    }
+
+    // console.log(digitosSumados + "esta es mi suma")
+
+    // validar que el residuo de 10 sea igual a cero
+    let validCard = false;
+    if (digitosSumados % 10 === 0) {
+      validCard = true;
+    } else {
+      validCard = false;
+    }
+    return validCard;
+  },
+
+  maskify(cardNumber) {
+    const arrayNumber = cardNumber.toString().split('');
+    for (let i = 0; i < arrayNumber.length-4 ; i++) {
+      arrayNumber[i] = "#";
+    }
+    return arrayNumber.join('');
+  },
+}
 
 
 export default validator;
